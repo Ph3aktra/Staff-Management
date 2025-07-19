5ceb51d5148405f4b59f65d4faec5f5be468ed51
TTAAAN <122733793+TTAAAN@users.noreply.github.com>
2025-07-19 15:48:27 +0700

    Improved formatting

--- Management.cpp
+++ Management.cpp
@@ -4,94 +4,113 @@
 #include "Management.h"
 using namespace std;
 
-Staff::Staff(){
-name = "N/a";
-age = 0;
-id = "N/A";
-gender = "N/A";
-department = "Unknown";
-position = "Unknown";
-year_started = -1;
-salary = -1;
-clock_in = "Unknown";
-clock_out = "Unknown";
-}
-Staff::Staff(string Name, int Age, string ID, string Gender,string Department, string Position, int Year_Started,int Salary, string in, string out){
-name = Name;
-age = Age;
-id = ID;
-gender = Gender;
-department = Department;
-position = Position;
-year_started = Year_Started;
-salary = Salary;
-clock_in = in;
-clock_out = out;
+Staff::Staff() {
+    name = "N/a";
+    age = 0;
+    id = "N/A";
+    gender = "N/A";
+    department = "Unknown";
+    position = "Unknown";
+    year_started = -1;
+    salary = -1;
+    clock_in = "Unknown";
+    clock_out = "Unknown";
 }
 
-void Staff::setName(string name){
-this -> name = name;
-}
-void Staff::setID(string id){
-this -> id = id;
-}
-void Staff::setGender(string gender){
-this -> gender = gender;
-}
-void Staff::setDept(string department){
-this -> department = department;
-}
-void Staff::setPosition(string position){
-this -> position = position;
-}
-void Staff::setClockIn(string clock_in){
-this -> clock_in = clock_in;
-}
-void Staff::setClockOut(string clock_out){
-this -> clock_out = clock_out;
-}
-void Staff::setAge(int age){
-this -> age = age;
-}
-void Staff::setSalary(int salary){
-this -> salary = salary;
-}
-void Staff::setYear(int year_started){
-this -> year_started = year_started;
+Staff::Staff(string Name, int Age, string ID, string Gender, string Department, string Position, int Year_Started, int Salary, string in, string out) {
+    name = Name;
+    age = Age;
+    id = ID;
+    gender = Gender;
+    department = Department;
+    position = Position;
+    year_started = Year_Started;
+    salary = Salary;
+    clock_in = in;
+    clock_out = out;
 }
 
-string Staff::getName(){
-return name;
-};
-string Staff::getID(){
-return id;
-};
-string Staff::getGender(){
-return gender;
-};
-string Staff::getDept(){
-return department;
-};
-string Staff::getPosition(){
-return position;
-};
-string Staff::getClockIn(){
-return clock_in;
-};
-string Staff::getClockOut(){
-return clock_out;
-};
-int Staff::getAge(){
-return age;
-};
-double Staff::getSalary(){
-return salary;
-};
-int Staff::getYear(){
-return year_started;
-};
+void Staff::setName(string name) {
+    this->name = name;
+}
 
-void Staff::displayProfile(){
+void Staff::setID(string id) {
+    this->id = id;
+}
+
+void Staff::setGender(string gender) {
+    this->gender = gender;
+}
+
+void Staff::setDept(string department) {
+    this->department = department;
+}
+
+void Staff::setPosition(string position) {
+    this->position = position;
+}
+
+void Staff::setClockIn(string clock_in) {
+    this->clock_in = clock_in;
+}
+
+void Staff::setClockOut(string clock_out) {
+    this->clock_out = clock_out;
+}
+
+void Staff::setAge(int age) {
+    this->age = age;
+}
+
+void Staff::setSalary(int salary) {
+    this->salary = salary;
+}
+
+void Staff::setYear(int year_started) {
+    this->year_started = year_started;
+}
+
+string Staff::getName() {
+    return name;
+}
+
+string Staff::getID() {
+    return id;
+}
+
+string Staff::getGender() {
+    return gender;
+}
+
+string Staff::getDept() {
+    return department;
+}
+
+string Staff::getPosition() {
+    return position;
+}
+
+string Staff::getClockIn() {
+    return clock_in;
+}
+
+string Staff::getClockOut() {
+    return clock_out;
+}
+
+int Staff::getAge() {
+    return age;
+}
+
+double Staff::getSalary() {
+    return salary;
+}
+
+int Staff::getYear() {
+    return year_started;
+}
+
+void Staff::displayProfile() {
     cout << "---------------------------------------------------------------------\n";
     cout << "\033[33mProfile: " << name << "\033[0m\n";
     cout << "---------------------------------------------------------------------\n";
@@ -117,9 +136,8 @@ void Staff::displayProfile(){
          << "Salary: $" << fixed << setprecision(2) << salary << endl;
 } 
 
-void Staff::saveToFile(ofstream& file){
-    file << name << "," << age << "," << id << "," << gender << "," 
+void Staff::saveToFile(ofstream& file) {
+    file << name << "," << age << "," << id << "," << gender << ","
          << department << "," << position << "," << year_started << "," 
          << salary << "," << clock_in << "," << clock_out << "\n";
-};
-
+}
